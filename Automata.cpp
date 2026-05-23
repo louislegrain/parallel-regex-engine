e2172bb6ff2edd62027970547ebaaaf8506e2748
Léonard Mauvernay <mauvernay.l@gmail.com>
2026-05-23 11:50:09 +0200

    NFA and DFA construction functions

--- Automata.cpp
+++ Automata.cpp
@@ -3,6 +3,14 @@
 #include <set>
 #include "Automata.h"
 
+// Useful for the McNaughton–Yamada–Thompson algorithm
+// See also
+// https://dmitrysoshnikov.medium.com/building-a-regexp-machine-part-2-finite-automata-nfa-fragments-5a7c5c005ef0
+struct NFAFragment {
+    int start;
+    int accept;
+};
+
 int DFA::step(int state, char c) const {
     if (state < 0 || state >= size()) {
         return -1;
@@ -22,8 +30,18 @@ bool DFA::accepts(const std::string& text) const {
 
 int DFA::size() const { return transitions.size(); }
 
+NFAFragment build_fragment(RegexNode* node, NFA& nfa) {
+    // To do
+}
+
 void NFA::build(const RegexNode* root) {
+    NFA nfa;
+    NFAFragment f = build_fragment(root, nfa);
+    nfa.start = f.start;
+    nfa.accept = f.accept;
+    return nfa;
 }
 
 void DFA::build(const NFA& nfa) {
+    // To do (Louis)
 }
